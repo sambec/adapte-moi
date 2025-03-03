6df5b30e676a107110ecb676881a3105d31af87b
sambec <sarahambec@gmail.com>
2025-03-03 13:55:56 +0100

    :hammer: routes generales

--- app/routes/generales.py
+++ app/routes/generales.py
@@ -9,4 +9,67 @@ def home():
     return render_template('index.html')
 
 
+# -------------------- COLLECTIONS --------------------
 
+@app.route("/collections")
+def collections():
+    collections = db.session.execute("SELECT id, title, user_id FROM collection").fetchall()
+    return render_template("pages/collections.html", collections=collections, sous_titre="Toutes les collections")
+
+
+@app.route("/collection/<int:id>")
+def collection_specifique(id):
+    collection = db.session.execute("SELECT id, title, user_id, description FROM collection WHERE id = :id", {"id": id}).fetchone()
+    films = db.session.execute(
+        "SELECT film.id, film.title FROM film "
+        "JOIN collection_film ON film.id = collection_film.id_film "
+        "WHERE collection_film.id_collection = :id",
+        {"id": id}
+    ).fetchall()
+    if not collection:
+        return render_template("pages/404.html"), 404
+    return render_template("pages/collection.html", collection=collection, films=films, sous_titre=collection.title)
+
+
+# -------------------- LIVRES --------------------
+
+@app.route("/livres")
+def livres():
+    livres = db.session.execute("SELECT id, title, author FROM book").fetchall()
+    return render_template("pages/livres.html", livres=livres, sous_titre="Tous les livres")
+
+
+@app.route("/livre/<string:id>")
+def livre_specifique(id):
+    livre = db.session.execute("SELECT id, title, author FROM book WHERE id = :id", {"id": id}).fetchone()
+    films = db.session.execute(
+        "SELECT film.id, film.title FROM film "
+        "JOIN book_film ON film.id = book_film.id_film "
+        "WHERE book_film.id_livre = :id",
+        {"id": id}
+    ).fetchall()
+    if not livre:
+        return render_template("pages/404.html"), 404
+    return render_template("pages/livre.html", livre=livre, films=films, sous_titre=livre.title)
+
+
+# -------------------- FILMS --------------------
+
+@app.route("/films")
+def films():
+    films = db.session.execute("SELECT id, title, director FROM film").fetchall()
+    return render_template("pages/films.html", films=films, sous_titre="Tous les films")
+
+
+@app.route("/film/<string:id>")
+def film_specifique(id):
+    film = db.session.execute("SELECT id, title, director FROM film WHERE id = :id", {"id": id}).fetchone()
+    livres = db.session.execute(
+        "SELECT book.id, book.title FROM book "
+        "JOIN book_film ON book.id = book_film.id_livre "
+        "WHERE book_film.id_film = :id",
+        {"id": id}
+    ).fetchall()
+    if not film:
+        return render_template("pages/404.html"), 404
+    return render_template("pages/film.html", film=film, livres=livres, sous_titre=film.title)
---
+++ app/routes/users.py
@@ -0,0 +1,25 @@
+from ..app import app, db
+from flask import render_template, request, flash, redirect, url_for, abort
+
+# from ..models.formulaires import Recherche
+
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+# -------------------- UTILISATEURS --------------------
+
+@app.route("/utilisateurs")
+def utilisateurs():
+    utilisateurs = db.session.execute("SELECT id, name, email FROM user").fetchall()
+    return render_template("pages/utilisateurs.html", utilisateurs=utilisateurs, sous_titre="Tous les utilisateurs")
+
+
+@app.route("/utilisateur/<int:id>")
+def utilisateur_specifique(id):
+    utilisateur = db.session.execute("SELECT id, name, email FROM user WHERE id = :id", {"id": id}).fetchone()
+    if not utilisateur:
+        return render_template("pages/404.html"), 404
+    return render_template("pages/utilisateur.html", utilisateur=utilisateur, sous_titre=utilisateur.name)
+
