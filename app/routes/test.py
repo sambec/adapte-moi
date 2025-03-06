61d3fc69599c9be28e52e72e9ea463e53d446e5b
desireesdata <joel.feral@sfr.fr>
2025-03-06 14:43:01 +0100

    [TESTS] Feature pour la recherche via formulaire ou url

--- app/routes/test.py
+++ app/routes/test.py
@@ -1,5 +1,5 @@
 from ..app import app, db
-from flask import render_template, request, flash, redirect, url_for
+from flask import render_template, request, flash, redirect, url_for, request
 from sqlalchemy import or_
 from ..models.adapte_moi import Film, Book
 # from ..models.formulaires import Recherche
@@ -10,14 +10,37 @@ def test_book():
     books = []
     for book in Book.query.all():
         books.append(book.title)
-    return render_template("pages/index.html", livres=books)
+    return render_template("pages/test.html", livres=books)
+
+@app.route("/books/<string:book_name>")
+def get_book(book_name):
+    book = Book.query.filter_by(title=book_name).first()
+    if book:
+        return render_template("pages/test.html", livre=book.title, auteur=book.author)
+    else:
+        return "Livre non trouvé", 404
+    
+@app.route("/recherche", methods=['GET', 'POST'])
+def recheche():
+    titles = ""
+    if request.method == "POST":
+        donnees = request.form
+        print(donnees)
+        my_title = donnees.get("title")
+        if my_title:
+            titles = Book.query.filter(Book.title.like(f"%{my_title}%")).all()
+        else :
+            titles = "rieng"
+    else :
+        pass
+    return render_template("pages/test.html", titres=titles)
 
 @app.route("/test_movie")
 def test_movie():
     movies = []
     for movie in Film.query.all():
         movies.append(movie.title)
-    return render_template("pages/index.html", films=movies)
+    return render_template("pages/test.html", films=movies)
 
 
 
